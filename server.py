5453641189d08595203c1f6c8678eeb8ceac9a26
Esvin Joshua <72260184+devjoshua312@users.noreply.github.com>
2022-12-24 14:22:18 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,67 +9,70 @@ def authCheck(key):
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	return("Home Page")
-
-
-@app.route("/image")
-def image_gen():
-	param = request.args.get("param")
-	kkey = request.args.get("api_key")
-	if authCheck(kkey) == "ok":
-		try:
-			img = image(param)
-			return(img)
-		except Exception as e:
-			return(f"{e}")
-	else:
-		return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'image', key: {param}")
-
-
-@app.route("/chat")
-def chat_bot():
-	kkey = request.args.get("api_key")
-	text = request.args.get("text")
-	if authCheck(kkey) == "ok":
-		try:
-			msg = friend(text)
-			return(msg)
-		except Exception as e:
-			return(f"{e}")
-	else:
-		return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'chat', key: {text}")
-
-
-@app.route("/name")
-def name_gen():
-	kkey = request.args.get("api_key")
-	desc = request.args.get("des")
-	seed = request.args.get("seed")
-	if authCheck(kkey) == "ok":
-		try:
-			prd = name(desc, seed)
-			return(prd)
-		except Exception as e:
-			return(f"{e}")
-	else:
-		return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'name generator', key: {desc}, seed: {seed}")
-
-@app.route("/horror")
-def scare():
-	topic = request.args.get("top")
-	kkey = request.args.get("api_key")
-	if authCheck(kkey) == "ok":
-		try:
-			story = horror(topic)
-			return(story)
-		except Exception as e:
-			return(f"{e}")
-	else:
-		return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'horror story gen', key: {topic}")
-
-
-
+try:
+	
+	@app.route("/")
+	def home():
+		return("Home Page")
+	
+	
+	@app.route("/image")
+	def image_gen():
+		param = request.args.get("param")
+		kkey = request.args.get("api_key")
+		if authCheck(kkey) == "ok":
+			try:
+				img = image(param)
+				return(img)
+			except Exception as e:
+				return(f"{e}")
+		else:
+			return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'image', key: {param}")
+	
+	
+	@app.route("/chat")
+	def chat_bot():
+		kkey = request.args.get("api_key")
+		text = request.args.get("text")
+		if authCheck(kkey) == "ok":
+			try:
+				msg = friend(text)
+				return(msg)
+			except Exception as e:
+				return(f"{e}")
+		else:
+			return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'chat', key: {text}")
+	
+	
+	@app.route("/name")
+	def name_gen():
+		kkey = request.args.get("api_key")
+		desc = request.args.get("des")
+		seed = request.args.get("seed")
+		if authCheck(kkey) == "ok":
+			try:
+				prd = name(desc, seed)
+				return(prd)
+			except Exception as e:
+				return(f"{e}")
+		else:
+			return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'name generator', key: {desc}, seed: {seed}")
+	
+	@app.route("/horror")
+	def scare():
+		topic = request.args.get("top")
+		kkey = request.args.get("api_key")
+		if authCheck(kkey) == "ok":
+			try:
+				story = horror(topic)
+				return(story)
+			except Exception as e:
+				return(f"{e}")
+		else:
+			return(f"incorrect or non-existent api key. please check your url. debug info: Service called: 'horror story gen', key: {topic}")
+except Exception as e:
+	print(f"{e}")
+	
+	
 if __name__ == "__main__":
 	app.run(debug=True)
